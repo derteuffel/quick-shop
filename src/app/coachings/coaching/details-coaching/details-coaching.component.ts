792902c2d92f693735c214af5e147cff86eb25b0
EDOUGA jean patrick <edougajean@gmail.com>
2021-05-27 18:04:02 +0200

    sessioncoaching 27/05/2021

--- src/app/coachings/coaching/details-coaching/details-coaching.component.ts
+++ src/app/coachings/coaching/details-coaching/details-coaching.component.ts
@@ -15,28 +15,16 @@ import {Coaching} from "../../../models/coaching";
   providers: [MessageService],
 })
 export class DetailsCoachingComponent implements OnInit {
-
-  public currentCoacing:any = {
-    title: 'Titre',
-    description: 'description',
-    email: 'Email',
-    phone1: 'phone1',
-    phone: 'phone',
-    amount: 100000.0,
-    logo: '',
-    region: 'Mokolo'
-  };
+  message: string;
+  loading = true;
   public submitted = false;
-  sessions: SessionCoaching[];
-  sessionID;
-  currentSession: SessionCoaching;
-  sessionForm: FormGroup;
-
   p = 1;
-
-  public searchItem:any = {
-
-  };
+  searchItem: string;
+  currentCoaching: Coaching;
+  sessions: SessionCoaching[];
+  currentSession: SessionCoaching;
+  session: SessionCoaching;
+  sessionForm: FormGroup;
 
   constructor(private coachingService: CoachingService,
               private fb: FormBuilder,
@@ -47,89 +35,65 @@ export class DetailsCoachingComponent implements OnInit {
               private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.loadDatas();
-    this.loadSession();
+    this.loadList();
+    this.getSession(this.activatedRoute.snapshot.paramMap.get('id'));
     this.initForm();
   }
 
-  private loadDatas(){
-    this.coachingService.getCoachingById(this.activatedRoute.snapshot.paramMap.get('id'))
-    .subscribe(data=>{
-      this.currentCoacing = data;
-      console.log(data);
-    }, error => {
+  /** Lister les sessions d'un coaching **/
 
-    });
-  }
-  /** lister les sessions d'un coaching **/
-  loadSession(){
-    this.sessionService.getSessionCoaching(this.activatedRoute.snapshot.paramMap.get('id')).subscribe(
+  loadList(): void{
+    this.sessionService.getAllSessionCoaching().subscribe(
       (res: any) => {
         this.sessions = res;
+      },
+      error1 => {
+        console.log(error1);
+      }
+    );
+  }
+  getSession(id){
+    this.coachingService.getCoachingById(id).subscribe(
+      data => {
+        this.currentCoaching = data;
       }, error1 => {
         console.log(error1);
       }
     );
-
   }
 
-  initForm() {
-    this.sessionForm = new FormGroup({
-      id: new FormControl(''),
-      label: new FormControl(''),
-      startDate: new FormControl(''),
-      endDate: new FormControl(''),
-      coaching: new FormControl('')
-    })
-  }
 
   openModalSession(contentAdd){
     this.modalService.open(contentAdd, {size: 'lg'});
   }
 
-  addNewSession(){
+initForm() {
+    this.sessionForm = new FormGroup({
+      id: new FormControl(''),
+      label: new FormControl(''),
+      startDate: new FormControl(''),
+      endDate: new FormControl(''),
+    })
+}
+
+  // fonction d'ajout d'une session
+
+  onSubmitSession() {
     this.submitted = true;
-    if(this.sessionForm?.invalid) return;
-    console.log(this.activatedRoute.snapshot.paramMap.get('id'));
-
-    console.log(this.sessionForm);
-    this.sessionService.createSession(this.sessionForm.value, this.activatedRoute.snapshot.paramMap.get('id')).subscribe(
-      data =>{
-        this.messageService.add({severity: 'success', summary: 'Success', detail: 'article submitted', sticky: true});
-        this.loadSession();
+    if(this.sessionForm?.invalid){return;}
+    this.sessionService.createSession(this.sessionForm?.value, this.activatedRoute.snapshot.paramMap.get('id')).subscribe(
+      data => {
         this.sessionForm.reset();
-
+        this.messageService.add({severity: 'success', summary: 'Success', detail: 'article submitted', sticky: true});
+        this.loadList();
+        window.location.reload();
       },
       error => {
         this.messageService.add({severity: 'error', summary: 'Error', detail: 'Message Content'});
         console.log(error);
       }
-    )
-
+    );
   }
-
-  newSession(){
-
-    this.submitted = true;
-    if(this.sessionForm?.invalid) return;
-
-
-    console.log(this.sessionForm);
-    this.sessionService.saveSessionCoaching(this.sessionForm.value).subscribe(
-      data =>{
-        this.messageService.add({severity: 'success', summary: 'Success', detail: 'article submitted', sticky: true});
-        this.loadSession();
-        this.sessionForm.reset();
-
-      },
-      error => {
-        this.messageService.add({severity: 'error', summary: 'Error', detail: 'Message Content'});
-        console.log(error);
-      }
-    )
-
-  }
-
   /** toast message function primeng  **/
   onConfirm() {
     this.messageService.clear('c');
