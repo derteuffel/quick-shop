29a098a7482cd195be61dfc152f3740d69abb38c
EDOUGA jean patrick <edougajean@gmail.com>
2021-05-30 17:06:55 +0100

    ajout formulaire dans détail coaching du home

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,41 @@
 export const environment = {
   production: true
 };
+
+
+const url = 'http://localhost:8181/';
+
+export const API = {
+
+
+  /** Produits **/
+
+
+  PRODUITS:  `${url}api/produits`,
+
+  /** Commandes **/
+
+  COMMANDES: `${url}api/commandes`,
+
+
+  /** Boutiques **/
+
+  BOUTIQUES: `${url}api/boutiques`,
+
+  /** Coachings **/
+
+  COACHINGS: `${url}api/coachings`,
+
+  /** Account **/
+
+  ACCOUNT: `${url}api/account`,
+
+  /** Micro-finance **/
+
+  MICROFINANCES: `${url}api/microfinancements`,
+
+  /** Session coaching**/
+
+  SESSIONS: `${url}api/sessioncoachings`,
+
+}
