b199056da7a0134f99a5ec1bc9544393dc9b4b78
EDOUGA jean patrick <edougajean@gmail.com>
2021-06-09 17:51:59 +0100

    remove dependance in app.module and app.routing.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,10 +16,6 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
 import { DetailProductComponent } from './eco/product/detail-product/detail-product.component';
 import { CommandeComponent } from './admin/commande/commande/commande.component';
-import { BoutiqueComponent } from './admin/boutique/boutique/boutique.component';
-import { AddBoutiqueComponent } from './admin/boutique/add-boutique/add-boutique.component';
-import { BoutiqueDetailComponent } from './admin/boutique/boutique-detail/boutique-detail.component';
-import { UpdateBoutiqueComponent } from './admin/boutique/update-boutique/update-boutique.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
@@ -46,7 +42,6 @@ import { SingupComponent } from './auth/singup/singup.component';
 import { TestComponent } from './test/test/test.component';
 import {AdminFooterComponent} from "./admin/admin-footer/admin-footer.component";
 import {AdminSidebarComponent} from "./admin/admin-sidebar/admin-sidebar.component";
-import { ArticleComponent } from './admin/boutique/article/article.component';
 import { ArticlesComponent } from './eco/articles/articles.component';
 import { CoachingsComponent } from './eco/coachings/coachings.component';
 import { CoachingDetailComponent } from './eco/coachings/coaching-detail/coaching-detail.component';
@@ -83,10 +78,6 @@ import { AccountComponent } from './account/account.component';
     AdminHeaderComponent,
     DetailProductComponent,
     CommandeComponent,
-    BoutiqueComponent,
-    AddBoutiqueComponent,
-    BoutiqueDetailComponent,
-    UpdateBoutiqueComponent,
     LoginComponent,
     RegisterComponent,
     UnauthorizedComponent,
@@ -100,7 +91,6 @@ import { AccountComponent } from './account/account.component';
     TestComponent,
     AdminFooterComponent,
     AdminSidebarComponent,
-    ArticleComponent,
     ArticlesComponent,
     CoachingsComponent,
     CoachingsSearchComponent,
