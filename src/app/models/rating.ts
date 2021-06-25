b18c3e96c108fae6e938640ee38197edcd0e00b5
EDOUGA jean patrick <edougajean@gmail.com>
2021-06-25 18:24:51 +0100

    rating

--- src/app/models/rating.ts
+++ src/app/models/rating.ts
@@ -1,2 +1,11 @@
+import {Product} from "./product.model";
+import {User} from "./user";
+
 export class Rating {
+
+  id: number;
+  product: Product;
+  rating: number;
+  comments: string;
+  user: User;
 }
