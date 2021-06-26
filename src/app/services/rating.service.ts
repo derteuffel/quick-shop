69e84a8948b627ee201b580af19a0491505c7337
EDOUGA jean patrick <edougajean@gmail.com>
2021-06-26 08:15:08 +0100

    test

--- src/app/services/rating.service.ts
+++ src/app/services/rating.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/index";
+import {API} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RatingService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  submitUserFeedback(pid,uid,rating): Observable<any>  {
+
+    return this.http.post(`${API.RATING}/feedback/${pid}/${uid}/${rating}`,rating);
+  }
 }
